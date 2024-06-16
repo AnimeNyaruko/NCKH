7170a88e9953c80248c7aec901abfdfeda114d37
AnimeNyaruko <robloxandminecraft1603@gmail.com>
2024-06-16 22:49:27 +0700

    Main Page Completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
 import Image from 'next/image';
+import LoginButton from './ui/Buttons/Login';
+import UserIcon from '@/public/images/UserIcon.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function Home() {
 	return (
@@ -32,6 +35,22 @@ export default function Home() {
 							className="block relative"
 							style={{ objectFit: 'contain' }}
 						/>
+					</div>
+					<div className="w-auto h-full ml-auto mr-[2.5rem] flex items-center">
+						<LoginButton
+							className="relative block cursor-pointer mr-[2.5rem]"
+							url="/login"
+							placeholder="LOG IN"
+						/>
+						<div className="relative flex items-center h-full w-fit">
+							<Image
+								src={UserIcon}
+								alt="User Icon"
+								loading="eager"
+								className="relative h-[90%] w-auto cursor-pointer"
+								style={{ objectFit: 'contain' }}
+							/>
+						</div>
 					</div>
 				</div>
 			</div>
--- app/ui/Buttons/Login.tsx
+++ app/ui/Buttons/Login.tsx
@@ -1,1 +1,27 @@
-export default function LoginButton() {}
+import clsx from 'clsx';
+import Link from 'next/link';
+
+export default function LoginButton({
+	placeholder,
+	className,
+	url,
+	...rest
+}: {
+	placeholder: string;
+	className?: string;
+	url: string;
+}) {
+	return (
+		<Link
+			{...rest}
+			href={url}
+			className={clsx(
+				'w-max h-min flex items-center justify-center hover:drop-shadow-[0_0_0.25rem_rgba(0,0,0,0.25)] rounded-2xl bg-white',
+				className
+			)}>
+			<p className="relative block w-max h-min py-[0.25rem] px-[2.5rem] m-[0.1rem] roboto font-bold text-center text-[#408AFF] text-[12.5px] border-solid border-[1px] rounded-2xl">
+				{placeholder}
+			</p>
+		</Link>
+	);
+}
